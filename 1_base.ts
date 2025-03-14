d8cefeabd5799a34a4adf2cf147a8aaf472c69e0
Anton Chepelev <chepelash@gmail.com>
2025-03-14 22:40:18 +0300

    feat: complete homework

--- 1_base.ts
+++ 1_base.ts
@@ -1,24 +1,32 @@
 /**
  * Нужно превратить файл в ts и указать типы аргументов и типы возвращаемого значения
  * */
-export const removePlus = (string) => string.replace(/^\+/, '');
+export const removePlus = (string: string): string => string.replace(/^\+/, "");
 
-export const addPlus = (string) => `+${string}`;
+export const addPlus = (string: string): string => `+${string}`;
 
-export const removeFirstZeros = (value) => value.replace(/^(-)?[0]+(-?\d+.*)$/, '$1$2');
+export const removeFirstZeros = (value: string): string =>
+  value.replace(/^(-)?[0]+(-?\d+.*)$/, "$1$2");
 
-export const getBeautifulNumber = (value, separator = ' ') =>
+export const getBeautifulNumber = (
+  value?: string,
+  separator = " ",
+): string | undefined =>
   value?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
 
-export const round = (value, accuracy = 2) => {
+export const round = (value: number, accuracy = 2): number => {
   const d = 10 ** accuracy;
   return Math.round(value * d) / d;
 };
 
-const transformRegexp =
+const transformRegexp: RegExp =
   /(matrix\(-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, )(-?\d+(\.\d+)?), (-?\d+(\.\d+)?)\)/;
 
-export const getTransformFromCss = (transformCssString) => {
+type Point = {
+  x: number;
+  y: number;
+};
+export const getTransformFromCss = (transformCssString: string): Point => {
   const data = transformCssString.match(transformRegexp);
   if (!data) return { x: 0, y: 0 };
   return {
@@ -27,20 +35,22 @@ export const getTransformFromCss = (transformCssString) => {
   };
 };
 
-export const getColorContrastValue = ([red, green, blue]) =>
+export const getColorContrastValue = ([red, green, blue]: number[]): number =>
   // http://www.w3.org/TR/AERT#color-contrast
   Math.round((red * 299 + green * 587 + blue * 114) / 1000);
 
-export const getContrastType = (contrastValue) => (contrastValue > 125 ? 'black' : 'white');
+export const getContrastType = (contrastValue: number): string =>
+  contrastValue > 125 ? "black" : "white";
 
 export const shortColorRegExp = /^#[0-9a-f]{3}$/i;
 export const longColorRegExp = /^#[0-9a-f]{6}$/i;
 
-export const checkColor = (color) => {
-  if (!longColorRegExp.test(color) && !shortColorRegExp.test(color)) throw new Error(`invalid hex color: ${color}`);
+export const checkColor = (color: string): void => {
+  if (!longColorRegExp.test(color) && !shortColorRegExp.test(color))
+    throw new Error(`invalid hex color: ${color}`);
 };
 
-export const hex2rgb = (color) => {
+export const hex2rgb = (color: string): number[] => {
   checkColor(color);
   if (shortColorRegExp.test(color)) {
     const red = parseInt(color.substring(1, 2), 16);
@@ -54,12 +64,36 @@ export const hex2rgb = (color) => {
   return [red, green, blue];
 };
 
-export const getNumberedArray = (arr) => arr.map((value, number) => ({ value, number }));
-export const toStringArray = (arr) => arr.map(({ value, number }) => `${value}_${number}`);
+type ValueNumber = {
+  value: string;
+  number: number;
+};
+export const getNumberedArray = (arr: string[]): ValueNumber[] =>
+  arr.map((value, number) => ({ value, number }));
+export const toStringArray = (arr: ValueNumber[]): string[] =>
+  arr.map(({ value, number }) => `${value}_${number}`);
 
-export const transformCustomers = (customers) => {
-  return customers.reduce((acc, customer) => {
-    acc[customer.id] = { name: customer.name, age: customer.age, isSubscribed: customer.isSubscribed };
-    return acc;
-  }, {});
+type Customer = {
+  id: number;
+  name: string;
+  age: number;
+  isSubscribed: boolean;
+};
+
+type CustomerAcc = Omit<Customer, "id">;
+
+export const transformCustomers = (
+  customers: Customer[],
+): { [x: string]: CustomerAcc } => {
+  return customers.reduce(
+    (acc: { [x: string]: CustomerAcc }, customer: Customer) => {
+      acc[customer.id] = {
+        name: customer.name,
+        age: customer.age,
+        isSubscribed: customer.isSubscribed,
+      };
+      return acc;
+    },
+    {},
+  );
 };
